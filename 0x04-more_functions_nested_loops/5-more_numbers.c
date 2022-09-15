d9742d430b60403387711ae1c77825bb03d2e504
uchechi <nwabiaesther@gmail.com>
2022-09-15 04:11:53 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,9 +12,9 @@ void more_numbers(void)
 	{
 		for (k = 0; k <= 14; k++)
 		{
-			if (j >= 10)
-				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
+			if (k >= 10)
+				_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
 		}
 		_putchar('\n');
 	}

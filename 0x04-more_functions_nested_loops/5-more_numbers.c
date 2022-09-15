5f16f532bc03b963f092f905d9dc848fe5160696
uchechi <nwabiaesther@gmail.com>
2022-09-15 04:01:54 -0700

    0-5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
+ */
+void more_numbers(void)
+{
+	int i, k;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (k = 0; k <= 14; k++)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+void print_line(int n)
+{
+
+}

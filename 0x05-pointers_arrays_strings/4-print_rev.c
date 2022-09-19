a7ce9d5f5b1800d5f71d6dbc24f5b563e9a382b1
uchechi <nwabiaesther@gmail.com>
2022-09-19 13:46:08 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,21 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int len = 0;
+        int index = 0;
+	char temp;
 
 	while (s[index++])
 		len++;
 
-	for (index = len; index >= 0; index--)
-		_putchar(s[index]);
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		tmp = s[index];
+		s[index] = s[len - index - 1];
+		s[len - index - 1] = tmp;
+
+		_putchar(tmp);
+	}
 
 	_putchar('\n');
 }

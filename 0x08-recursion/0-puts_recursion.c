938eaf1cca4342a4d2b250cd640d64df89fe94ee
uchechi <nwabiaesther@gmail.com>
2022-09-28 09:36:30 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,10 @@
 */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-	else
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
-	}
-
-	_putchar('\n');
+	} else
+		_putchar('\n');
 }

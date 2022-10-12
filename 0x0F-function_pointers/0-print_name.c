3d1ee796605606673724e41e80aaa6e58441c2cb
uchechi <nwabiaesther@gmail.com>
2022-10-12 09:57:59 -0700

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+* print_name - a function that prints a name.
+* @name: the name to be printed.
+* @f: pointer to a function.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
